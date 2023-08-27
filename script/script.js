b4e6cb1272edee23a7208ce28b551ce69661d214
VladimirMakarof <stan13.09@mail.ru>
2023-08-27 22:56:10 +0300

    add fotos and quote

--- script/script.js
+++ script/script.js
@@ -4,6 +4,10 @@ headers.append("apikey", apiKey);
 
 const BASE_URL = 'https://htnkwsplrnasfyxutazx.supabase.co/rest/v1';
 
+
+const canvas = document.getElementById("image-canvas");
+const ctx = canvas.getContext("2d");
+
 let global_data = {
   motivation: {
     long: [],
@@ -78,3 +82,166 @@ fetchAndDisplayQuotes("motivation", "short", "short");
 fetchAndDisplayQuotes("motivation", "long", "long");
 fetchAndDisplayQuotes("love", "short", "love-short");
 fetchAndDisplayQuotes("love", "long", "love-long");
+
+// фото  API 
+
+const accessKey = 'dfN4bFrAAlW5zZtS3d03Y1ycn0XzfERHewdopG9nZm8';
+const endpoint = 'https://api.unsplash.com/photos/random';
+const imageContainer = document.getElementById('image-container');
+
+function fetchRandomImage(query, collections, orientation, size) {
+  fetch(`${endpoint}?query=${query}&collections=${collections}&orientation=${orientation}&fit=clip&w=${size}&h=${size}`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Client-ID ${accessKey}`
+    }
+  })
+    .then(response => response.json())
+    .then(data => {
+      const imageUrl = data.urls.regular;
+
+      // Создаем элемент img и устанавливаем ему URL
+      const imgElement = document.createElement('img');
+      imgElement.src = imageUrl;
+
+      // Очищаем контейнер и добавляем новый элемент
+      imageContainer.innerHTML = '';
+      imageContainer.appendChild(imgElement);
+
+      // Сохранение URL фотографии в локальное хранилище
+      localStorage.setItem('selectedImage', imageUrl);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
+
+
+
+document.getElementById('motivation-horizontal-small').addEventListener('click', () => {
+  fetchRandomImage('motivation', '825815', 'landscape', 300);
+});
+
+document.getElementById('motivation-horizontal-large').addEventListener('click', () => {
+  fetchRandomImage('motivation', '825815', 'landscape', 1200);
+});
+
+document.getElementById('motivation-vertical-small').addEventListener('click', () => {
+  fetchRandomImage('motivation', '825815', 'portrait', 300);
+});
+
+document.getElementById('motivation-vertical-large').addEventListener('click', () => {
+  fetchRandomImage('motivation', '825815', 'portrait', 1200);
+});
+
+document.getElementById('love-horizontal-small').addEventListener('click', () => {
+  fetchRandomImage('love', '208403', 'landscape', 300);
+});
+
+document.getElementById('love-horizontal-large').addEventListener('click', () => {
+  fetchRandomImage('love', '208403', 'landscape', 1200);
+});
+
+document.getElementById('love-vertical-small').addEventListener('click', () => {
+  fetchRandomImage('love', '208403', 'portrait', 300);
+});
+
+document.getElementById('love-vertical-large').addEventListener('click', () => {
+  fetchRandomImage('love', '208403', 'portrait', 1200);
+});
+
+
+function displaySavedImage() {
+  const selectedImage = localStorage.getItem('selectedImage');
+  if (selectedImage) {
+    const imgElement = document.createElement('img');
+    imgElement.src = selectedImage;
+    imageContainer.innerHTML = '';
+    imageContainer.appendChild(imgElement);
+  }
+}
+
+// При загрузке страницы отобразить сохраненную фотографию (если есть)
+displaySavedImage();
+
+// ====
+
+
+// Функция для отображения цитаты на фотографии
+// Функция для отображения цитаты на фотографии
+function displayQuoteOnImage(quote) {
+  const img = new Image();
+  img.src = canvas.toDataURL();
+
+  img.onload = () => {
+    canvas.width = img.width;
+    canvas.height = img.height;
+    ctx.drawImage(img, 0, 0);
+
+    ctx.fillStyle = "white";
+    ctx.font = "36px Arial";
+    ctx.textAlign = "center";
+
+    ctx.fillText(quote, canvas.width / 2, canvas.height / 2);
+  };
+}
+
+// Обновление цитаты и ее отображение на фотографии
+function updateAndDisplayQuote(category, quoteType, containerId) {
+  if (
+    global_data[category] &&
+    global_data[category][quoteType] &&
+    global_data[category][quoteType].length > 0
+  ) {
+    const container = document.getElementById(containerId);
+    const quoteIndex = Math.floor(Math.random() * global_data[category][quoteType].length);
+
+    if (global_data[category][quoteType][quoteIndex]) {
+      const quoteText = global_data[category][quoteType][quoteIndex].quote_text;
+      const existingQuoteElement = document.getElementById("quote-text");
+      if (existingQuoteElement) {
+        existingQuoteElement.innerHTML = `"${quoteText}"`;
+      } else {
+        const newQuoteElement = document.createElement("div");
+        newQuoteElement.id = "quote-text";
+        newQuoteElement.classList.add("quote");
+        newQuoteElement.innerHTML = `"${quoteText}"`;
+        container.appendChild(newQuoteElement);
+      }
+      displayQuoteOnImage(quoteText);
+    }
+  }
+}
+
+// Обработчики кнопок для обновления цитат и их отображения на фотографии
+refreshShortQuotesButton.addEventListener("click", () => {
+  updateAndDisplayQuote("motivation", "short", "short");
+});
+
+refreshLongQuotesButton.addEventListener("click", () => {
+  updateAndDisplayQuote("motivation", "long", "long");
+});
+
+refreshLoveShortQuotesButton.addEventListener("click", () => {
+  updateAndDisplayQuote("love", "short", "love-short");
+});
+
+refreshLoveLongQuotesButton.addEventListener("click", () => {
+  updateAndDisplayQuote("love", "long", "love-long");
+});
+
+// Инициализация: загрузка цитат и их отображение на фотографии при загрузке страницы
+updateAndDisplayQuote("motivation", "short", "short");
+updateAndDisplayQuote("motivation", "long", "long");
+updateAndDisplayQuote("love", "short", "love-short");
+updateAndDisplayQuote("love", "long", "love-long");
+
+// Инициализация: отображение начальной фотографии
+const initialImage = new Image();
+initialImage.src = "URL_TO_INITIAL_IMAGE"; // Замените на URL начальной фотографии
+
+initialImage.onload = () => {
+  canvas.width = initialImage.width;
+  canvas.height = initialImage.height;
+  ctx.drawImage(initialImage, 0, 0);
+};
